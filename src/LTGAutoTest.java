c2f869a0098f39a2402f62d42781fe0c2a081ff1
shbknuttel <shb.knuttel@gmail.com>
2019-11-05 09:39:03 +0100

    2nd WebDriver Automation for Long-Term Garage, Open web driver and visit website

---
+++ src/LTGAutoTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LTGAutoTest {
+    public static void main(String[] args) throws InterruptedException {
+        // Set the property for webdriver.chrome.driver , local path to your ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\IdeaProjects\\Sven\\chromedriver_win32\\chromedriver.exe");
+        // Create new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        // Visit Parking Cost Calculator
+        driver.get("http://www.shino.de/parkcalc/index.php");
+        Thread.sleep(4000);
+        // Close Browser
+        driver.quit();
+    }
+
+}
